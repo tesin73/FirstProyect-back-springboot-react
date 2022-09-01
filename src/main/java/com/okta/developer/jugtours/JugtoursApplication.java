adeeb4ae754bf3d7ac6f45a5824598308ea8dda3
tesin73 <teobaldongonzab@gmail.com>
2022-08-31 21:44:02 -0400

    frontend url

--- src/main/java/com/okta/developer/jugtours/JugtoursApplication.java
+++ src/main/java/com/okta/developer/jugtours/JugtoursApplication.java
@@ -3,6 +3,8 @@ package com.okta.developer.jugtours;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// frontend url: https://github.com/tesin73/front-springboot-react-firstProyect
+
 @SpringBootApplication
 public class JugtoursApplication {
 
